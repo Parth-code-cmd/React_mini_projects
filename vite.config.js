3ce0f67dd1ea651087f790fb1a4593d416890629
Parth Gabhane <parthravigabhane@gmail.com>
2026-08-07 20:24:39 +0000

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/React_mini_projects/',
+
   server: {
     port: 3000,
     host: '0.0.0.0',
     allowedHosts: true,
   },
+
   build: {
     outDir: 'build',
   },
+
   plugins: [react()],
+
   test: {
     environment: 'jsdom',
     globals: true,
